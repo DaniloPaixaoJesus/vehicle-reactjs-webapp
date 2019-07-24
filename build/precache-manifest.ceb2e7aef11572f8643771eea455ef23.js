4abba13c25fedb300d530cd0ee979dde493c0af0
Danilo GingaOne <danilo.paixao@gingaone.com>
2019-07-24 11:22:56 -0300

    add init dynamic code vehicle and websocket

--- build/precache-manifest.ceb2e7aef11572f8643771eea455ef23.js
+++ build/precache-manifest.ceb2e7aef11572f8643771eea455ef23.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "40a77e65db74a8eceb1d24f25ea83913",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
+    "revision": "33de56a9c3d6d0976874",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "2e0129063922e3bb197a",
-    "url": "/static/js/2.b41502e9.chunk.js"
+    "revision": "f83b7ccb549325d3f06e",
+    "url": "/static/js/2.191081a5.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "33de56a9c3d6d0976874",
+    "url": "/static/js/main.670d29f1.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
+//  import SockJsClient from 'react-stomp';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  vehicles;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+     // let exampleSocket = new WebSocket('ws://localhost:8085/livestatus-websocket');
+    // exampleSocket.onmessage = e => {
+    //   console.log(e.data)
+    // }
+
+      let socket = new SockJS('http://ec2-35-174-0-145.compute-1.amazonaws.com:8085/livestatus-websocket');
+      let stompClient = Stomp.over(socket);
+      stompClient.connect({}, function (frame) {
+	        stompClient.subscribe('/topic/status', function (data) {
+	        	var vehicleStatus = JSON.parse(data.body);
+            console.log(vehicleStatus);
+            // const { error, isLoaded, items } = this.state;
+            // items.map(item => {  
+            //   if(scoreJson.vin == item.vin){
+            //     return {scoreJson}
+            //   }
+            // });
+                
+	            
+	        });
+      });
+      
+    fetch("http://swedish-challenge.danilopaixao.com.br:8080/vehicle-service/api/v1/vehicles")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.vehicles = result;
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.vin}>
+              Vin: {item.vin} - {item.status}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+}
+
+const card = {
+  boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+  transition: '0.3s'
+}
+
+const container = {
+  padding: '2px 16px'
 }
 
 export default App;
